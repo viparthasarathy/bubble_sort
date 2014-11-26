d9f447baa20dc1c32fafbb8d5509e8bdd9b06cd8
Vidul Parthasarathy <viparthasarathy@gmail.com>
2014-11-26 15:37:44 -0800

    edit

--- bubble_sort_method.rb
+++ bubble_sort_method.rb
@@ -3,12 +3,17 @@ def bubble_sort(array)
 	while num_trials < array.length
 		first_element = 0
 		second_element = 1
+		swap_sucess = false
 		while first_element < array.length - num_trials
 			if array[first_element] > array[second_element]
 				array.insert(first_element, array.delete_at(second_element))
+				swap_success = true
 			end
 			first_element += 1
 			second_element += 1
+		end
+		unless swap_success
+			break
 		end
 		num_trials += 1
 	end
