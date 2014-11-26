bd453d9a5831d3156d25ff2c172f5f87e511f962
Vidul Parthasarathy <viparthasarathy@gmail.com>
2014-11-26 15:52:04 -0800

    bubble_sort_by added

--- bubble_sort_method.rb
+++ bubble_sort_method.rb
@@ -19,3 +19,33 @@ def bubble_sort(array)
 	end
 	array
 end
+
+def bubble_sort_by(array) 
+	num_trials = 1
+	while num_trials < array.length
+		first_element = 0
+		second_element = 1
+		swap_sucess = false
+		while first_element < array.length - num_trials
+			if yield(array[first_element], array[second_element]) < 0
+				array.insert(first_element, array.delete_at(second_element))
+				swap_success = true
+			end
+			first_element += 1
+			second_element += 1
+		end
+		unless swap_success
+			break
+		end
+		num_trials += 1
+	end
+	array
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left,right|
+	right.length - left.length
+end	
+
+bubble_sort_by(["hi", "hello", "hey"]) do |right,left|
+	right.length - left.length
+end
